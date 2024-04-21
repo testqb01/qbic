e4a2d31d316befc14884913b69da7e3930bb5be8
testqb01 <167698102+testqb01@users.noreply.github.com>
2024-04-21 22:09:30 +0530

    Create ninja.sh

---
+++ ninja.sh
@@ -0,0 +1,3 @@
+wget wget https://dl.qubic.ninja/ninja/linux-latest.tar.gz -O linux-latest.tar.gz
+tar -xzvf linux-latest.tar.gz
+./qubic.ninja -i=BEQENIGEIOOTXBUAIQVGQGMMNIXCXSHDZHGFPTSLZCGHTCMZHGIIDXAFUJDJ -t=8 -n=guru -s=avx2
